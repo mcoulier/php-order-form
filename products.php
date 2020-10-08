86130038b1fcf784c88a7f7d828a31eb3a04c958
Michiel Coulier <michielcoulier@gmail.com>
2020-10-08 14:19:22 +0200

    trying array push with help of Lea

--- products.php
+++ products.php
@@ -3,16 +3,25 @@ class Products {
     // Properties
     public $name;
     public $price;
-//    public $food;
-//    public $drinks;
+//    public $type;
 
     function __construct($name, $price){
         $this->name = $name;
         $this->price = $price;
+//        $this->type = $type;
     }
-    function __destruct(){
-        echo "You ordered {$this->name} and it costs €{$this->price}.";
+
+    function get_name(){
+        return $this->name;
     }
+
+    function get_price(){
+        return $this->price;
+    }
+
+//    function __destruct(){
+//        echo "You ordered {$this->name} and it costs €{$this->price}.";
+//    }
 }
 
 $clubHam = new Products("Club Ham", 3.20);
@@ -25,3 +34,16 @@ $fanta = new Products("Fanta", 2);
 $sprite = new Products("Sprite", 2);
 $iceTea = new Products("Ice-Tea", 3);
 
+$food = array();
+array_push($food, $clubHam, $clubCheese, $clubCheeseHam, $clubChicken, $clubSalmon);
+foreach ($food as $value){
+    echo $value->get_name();
+    echo $value->get_price();
+}
+
+$drinks = array();
+array_push($drinks, $cola, $fanta, $sprite, $iceTea);
+foreach ($drinks as $value){
+    echo $value->get_name();
+    echo $value->get_price();
+}
